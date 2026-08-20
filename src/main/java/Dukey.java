aa115dc648545fe34b40140ac808f782138f5838
Alfred <alfredting2005@gmail.com>
2026-08-21 02:59:12 +0800

    Level 0, Change Name and Add greet

---
+++ src/main/java/Dukey.java
@@ -0,0 +1,18 @@
+public class Dukey {
+    public static void main(String[] args) {
+        String LINE = "____________________________________________________________" ;
+        String banner = " ____        _              \n"
+                + "|  _ \\ _   _| | _____ _   _ \n"
+                + "| | | | | | | |/ / _ \\ | | |\n"
+                + "| |_| | |_| |   <  __/ |_| |\n"
+                + "|____/ \\__,_|_|\\_\\___|\\__, |\n"
+                + "                       |___/ \n";
+        System.out.println(LINE);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Dukey.");
+        System.out.println("What can I do for you");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
