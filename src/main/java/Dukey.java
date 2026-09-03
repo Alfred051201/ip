edefaae540bd0b7b0e6b137139c5864683d9917b
Alfred <alfredting2005@gmail.com>
2026-09-03 22:15:19 +0800

    Revise Dukey structure. Move the chatbot loop into run to follow the OOP structure

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -3,145 +3,157 @@ import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class Dukey {
-    private static void saveTasks(Storage storage, TaskList tasks, Ui ui) {
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+    private Parser parser;
+
+    public Dukey(String filePath) {
+        this.ui = new Ui();
+        this.parser = new Parser();
+        this.ui.showWelcome();
+
+        this.storage = new Storage(filePath);
         try {
-            storage.save(tasks);
+            this.tasks = storage.load();
+        } catch (FileNotFoundException e) {
+            this.ui.showLoadingError();
+            this.tasks = new TaskList();
         } catch (DukeyException e) {
-            ui.showError(e.getMessage());
+            this.ui.showError(e.getMessage());
+            this.tasks = new TaskList();
         }
     }
 
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        Parser parser = new Parser();
-        ui.showWelcome();
+    private void saveTasks() {
+        try {
+            this.storage.save(this.tasks);
+        } catch (DukeyException e) {
+            this.ui.showError(e.getMessage());
+        }
+    }
 
+    public void run() {
         boolean conversation = true;
         Scanner scanner = new Scanner(System.in);
-        TaskList tasks = new TaskList();
-
-        Storage storage = new Storage("src/main/data/dukey.txt");
-        try {
-            tasks = storage.load();
-        } catch (FileNotFoundException e) {
-            ui.showLoadingError();
-        } catch (DukeyException e) {
-            ui.showError(e.getMessage());
-        }
 
         while (conversation && scanner.hasNextLine()) {
             String userInput = scanner.nextLine();
-            Command command = parser.parseCommand(userInput);
+            Command command = this.parser.parseCommand(userInput);
 
-            ui.showLine();
+            this.ui.showLine();
 
             if (command == Command.BYE) {
-                ui.showBye();
-                saveTasks(storage, tasks, ui);
+                this.ui.showBye();
+                saveTasks();
                 conversation = false;
             } else if (command == Command.LIST) {
-                ui.showList(tasks);
+                this.ui.showList(this.tasks);
             } else if (command == Command.ON) {
                 try {
-                    ui.showTasksOnDate(tasks, parser.parseOnDate(userInput));
+                    this.ui.showTasksOnDate(this.tasks, this.parser.parseOnDate(userInput));
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 } catch (DateTimeParseException e) {
-                    ui.showError("Please use date format: yyyy-MM-dd");
+                    this.ui.showError("Please use date format: yyyy-MM-dd");
                 }
             } else if (command == Command.MARK) {
                 try {
-                    int taskNumber = parser.parseTaskNumber(userInput, Command.MARK,
+                    int taskNumber = this.parser.parseTaskNumber(userInput, Command.MARK,
                             "Please provide a task number to mark.");
-                    if (!tasks.isValidTaskNumber(taskNumber)) {
+                    if (!this.tasks.isValidTaskNumber(taskNumber)) {
                         throw new DukeyException("Please provide a valid task number.");
                     }
 
-                    Task task = tasks.get(taskNumber);
+                    Task task = this.tasks.get(taskNumber);
                     task.markAsDone();
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskMarked(task);
+                    saveTasks();
+                    this.ui.showTaskMarked(task);
                 } catch (NumberFormatException e) {
-                    ui.showError("Please provide a valid task number.");
+                    this.ui.showError("Please provide a valid task number.");
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 }
             } else if (command == Command.UNMARK) {
                 try {
-                    int taskNumber = parser.parseTaskNumber(userInput, Command.UNMARK,
+                    int taskNumber = this.parser.parseTaskNumber(userInput, Command.UNMARK,
                             "Please provide a task number to unmark.");
-                    if (!tasks.isValidTaskNumber(taskNumber)) {
+                    if (!this.tasks.isValidTaskNumber(taskNumber)) {
                         throw new DukeyException("Please provide a valid task number.");
                     }
 
-                    Task task = tasks.get(taskNumber);
+                    Task task = this.tasks.get(taskNumber);
                     task.markAsUndone();
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskUnmarked(task);
+                    saveTasks();
+                    this.ui.showTaskUnmarked(task);
                 } catch (NumberFormatException e) {
-                    ui.showError("Please provide a valid task number.");
+                    this.ui.showError("Please provide a valid task number.");
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 }
             } else if (command == Command.TODO) {
                 try {
-                    Task newTask = new Todo(parser.parseTodoDescription(userInput));
-                    tasks.add(newTask);
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskAdded(newTask, tasks);
+                    Task newTask = new Todo(this.parser.parseTodoDescription(userInput));
+                    this.tasks.add(newTask);
+                    saveTasks();
+                    this.ui.showTaskAdded(newTask, this.tasks);
 
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 }
 
             } else if (command == Command.DEADLINE) {
                 try {
-                    String[] parts = parser.parseDeadline(userInput);
+                    String[] parts = this.parser.parseDeadline(userInput);
                     Task newTask = new Deadline(parts[0], parts[1]);
-                    tasks.add(newTask);
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskAdded(newTask, tasks);
+                    this.tasks.add(newTask);
+                    saveTasks();
+                    this.ui.showTaskAdded(newTask, this.tasks);
 
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 } catch (DateTimeParseException e) {
-                    ui.showError("Please use deadline date/time format: yyyy-MM-dd HHmm");
+                    this.ui.showError("Please use deadline date/time format: yyyy-MM-dd HHmm");
                 }
 
             } else if (command == Command.EVENT) {
                 try {
-                    String[] parts = parser.parseEvent(userInput);
+                    String[] parts = this.parser.parseEvent(userInput);
                     Task newTask = new Event(parts[0], parts[1], parts[2]);
-                    tasks.add(newTask);
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskAdded(newTask, tasks);
+                    this.tasks.add(newTask);
+                    saveTasks();
+                    this.ui.showTaskAdded(newTask, this.tasks);
 
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 } catch (DateTimeParseException e) {
-                    ui.showError("Please use event date/time format: yyyy-MM-dd HHmm");
+                    this.ui.showError("Please use event date/time format: yyyy-MM-dd HHmm");
                 }
             } else if (command == Command.DELETE) {
                 try {
-                    int taskNumber = parser.parseTaskNumber(userInput, Command.DELETE,
+                    int taskNumber = this.parser.parseTaskNumber(userInput, Command.DELETE,
                             "Please provide a task number to delete.");
-                    if (!tasks.isValidTaskNumber(taskNumber)) {
+                    if (!this.tasks.isValidTaskNumber(taskNumber)) {
                         throw new DukeyException("Please provide a valid task number.");
                     }
 
-                    Task task = tasks.delete(taskNumber);
-                    saveTasks(storage, tasks, ui);
-                    ui.showTaskDeleted(task, tasks);
+                    Task task = this.tasks.delete(taskNumber);
+                    saveTasks();
+                    this.ui.showTaskDeleted(task, this.tasks);
                 } catch (NumberFormatException e) {
-                    ui.showError("Please provide a valid task number.");
+                    this.ui.showError("Please provide a valid task number.");
                 } catch (DukeyException e) {
-                    ui.showError(e.getMessage());
+                    this.ui.showError(e.getMessage());
                 }
             } else {
-                ui.showError("I'm sorry, but I don't know what that means :-(");
+                this.ui.showError("I'm sorry, but I don't know what that means :-(");
             }
 
-            ui.showLine();
+            this.ui.showLine();
         }
+    }
+
+    public static void main(String[] args) {
+        new Dukey("src/main/data/dukey.txt").run();
     }
 }
